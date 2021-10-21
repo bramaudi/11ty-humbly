052a389eb285617a2b4babaf05ef139bf4c6216a
Brama Udi <bramaudi.al@gmail.com>
2021-10-21 15:01:52 +0700

    Add prefix to url

--- .eleventy.js
+++ .eleventy.js
@@ -119,7 +119,7 @@ module.exports = function(eleventyConfig) {
     // You can also pass this in on the command line using `--pathprefix`
 
     // Optional (default is shown)
-    pathPrefix: "/",
+    pathPrefix: "/11ty-humbly/",
     // -----------------------------------------------------------------
 
     // Pre-process *.md files with: (default: `liquid`)
